1a6a595292e16459fec6104ffd7272e9d4419d99
HillaryJR <hillaryjeramin@gmail.com>
2021-04-10 16:44:07 +0530

    first commit paymentService

---
+++ PaymentService/src/main/java/com/sliit/payment/Main.java
@@ -0,0 +1,11 @@
+package com.sliit.payment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the new Payment Service");
+
+	}
+
+}
